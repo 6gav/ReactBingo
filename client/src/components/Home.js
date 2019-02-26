55e2a6bf3cfdf7be2ad275325c6517c1d7eaa7b6
Gavin Watson <gavset99@gmail.com>
2019-02-25 22:18:14 -0500

    Basic game joining added

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,102 @@
 import React, {Component} from 'react';
+import './Home.css';
+
 
 class Home extends Component{
+    state = {
+        card: null,
+        userId: null,
+        gameId: null,
+        gameError: null,
+    }
+
+    constructor(props){
+        super(props);
+
+        this.GetUser = this.GetUser.bind(this);
+        this.SearchForGame = this.SearchForGame.bind(this);
+        this.StartGame = this.StartGame.bind(this);
+    }
 
 
+    componentDidMount(){
+        var userId = localStorage.getItem('userId');
+        if(!localStorage.getItem('userId'))
+        {
+            this.GetUser();
+        }
+        else
+        {
+            this.setState({userId: userId});
+        }
+    }
+
+    SearchForGame(e){
+        e.preventDefault();
+
+        fetch('/api/JoinGame', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({gameId: this.state.gameId}),
+        })
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+            if(response.errorCode === 404){
+                this.setState({gameError: response.error});
+                return;
+            }
+            else if (response.errorCode === 0){
+                this.setState({gameError: 'Joining...'});
+                window.location = '/game/' + this.state.gameId;
+            }
+        });
+    }
+
+    GetUser(){
+        fetch('/api/getUserId')
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+
+            localStorage.setItem('userId', response.id);
+            this.setState({userId: response.id});
+        });
+    }
+
+    StartGame(e){
+        console.log('Starting game...');
+
+        fetch('/api/StartGame', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                },
+            body: JSON.stringify({userId: this.state.userId}),
+        })
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+
+            if(response.statusCode === 0){
+               window.location = '/game/' + response.gameId; 
+            }
+        });
+    }
+    
     render(){
         return (
             <div>
-                <p>Home page</p>
+                <h1>User id: {this.state.userId}</h1>
+                <form onSubmit={this.SearchForGame}>
+                    <input type="text" placeholder="Game Id" onChange={e => this.setState({gameId: e.target.value}) }/>
+                    <input type="submit"/>
+                </form>
+                <p>{this.state.gameError}</p>
+
+                <button onClick={this.StartGame}>Create game</button>
             </div>
         );
     }
--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const path = require('path');
 const express = require('express');
-
+const bodyParser = require('body-parser');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io');
 io = io.listen(server);
 
+
+//Game directory
+var GameList = {};
+
+
+//Setup
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+app.set('port', (process.env.PORT || 5000));
+
+
+//Socket commands
 io.on('connection', (client) => {
     console.log('Client connected: ');
     console.log(client.id)
@@ -16,16 +28,56 @@ io.on('connection', (client) => {
 
 io.on('disconnect', (client) => {
     console.log('Client disconnected: ');
-    console.log(client.id);
+    console.log(client);
 });
 
 
-app.set('port', (process.env.PORT || 5000));
+//REST APIs
 
+app.get('/api/getUserId', (req, res) => {
+    var userId = Math.trunc(Math.random() * 100);
+    res.send({id: userId});
+});
+
+app.post('/api/JoinGame', (req, res) => {
+    var gameId = req.body.gameId;
+    if(!GameList[gameId]){
+        console.log(gameId + " does not exist!");
+        res.send({error: 'Game does not exist', errorCode: 404});
+    }
+    else{
+        console.log(GameList[gameId]);
+        res.send({error: 'Accepted', errorCode: 0});
+    }
+
+});
+
+app.post('/api/StartGame', (req, res) => {
+
+    var gameId;
+    do{
+        gameId = Math.floor(Math.random() * 10);
+    }while(GameList[gameId])
+
+    GameList[gameId] = {Host: req.body.userId};
+
+    console.log('Game with id ' + gameId + ' hosted by ' + req.body.userId + ' started.');
+    res.send({statusCode: 0, gameId: gameId});
+
+});
+
+
+
+//End REST
+
+
+
+//Render
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
+//Listen
 server.listen(app.get('port'), function() {
     console.log('App is listening on port: ' + app.get('port'));
 });
